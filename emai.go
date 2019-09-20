c385b23dba5da906fec97a14241ce83c130539ee
tanganyu1114 <48984491+tanganyu1114@users.noreply.github.com>
2019-09-20 10:56:59 +0800

    Update emai.go
    添加支持1-2行表头

--- emai.go
+++ emai.go
@@ -8,6 +8,7 @@ import (
 	"gopkg.in/gomail.v2"
 	"html/template"
 	"io/ioutil"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
@@ -18,8 +19,10 @@ var (
 	Emailconfig EmailConfig
 	Judge       string = "YES"
 	Sheetname   string
-	Entry       []string
+	Entry       [][]string
 	Enum        int
+	Nmail       int = 0
+	Head        int = 1
 )
 
 type EmailConfig struct {
@@ -37,7 +40,15 @@ type EmailConfig struct {
 	Xlsinfo struct {
 		Xls     string `json:"xls"`
 		Sheetid string `json:"sheetid"`
+		Head    string `json:"head"`
 	}
+}
+
+type Sendinfo struct {
+	Sbody	template.HTML
+	Spre   string
+	Ssuf   string
+	Stime  string
 }
 
 func main() {
@@ -45,7 +56,6 @@ func main() {
 	xlsfile := readXls()
 	echoInfo()
 	sendMail(xlsfile)
-
 }
 
 func echoInfo() {
@@ -60,8 +70,8 @@ func echoInfo() {
 		fmt.Println("邮件发送开始 ！")
 	} else if strings.ToUpper(Judge) == "Y" {
 		fmt.Println("邮件发送开始 ！")
-	}else {
-		panic("取消发送")
+	} else {
+		os.Exit(0)
 	}
 
 }
@@ -75,7 +85,8 @@ func readConfig() {
 
 func sendMail(rows [][]string) {
 	var touser string
-	for _, row := range rows[1:] {
+
+	for _, row := range rows[Head:] {
 		buffer := new(bytes.Buffer)
 		matched, err := regexp.MatchString("\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}", row[len(row)-1])
 		if matched {
@@ -86,31 +97,65 @@ func sendMail(rows [][]string) {
 			continue
 		}
 		checkErr(err)
-		t, _ := template.ParseFiles("./email_template.html")
-		t.Execute(buffer, struct {
-			SEntry []string
-			Srow   []string
-			Spre   string
-			Ssuf   string
-			Stime  string
-		}{
-			SEntry: Entry,
-			Srow:   row,
+		body:=template.HTML(Emailbody(Entry,row))
+
+		Sendinfo := Sendinfo{
+			Sbody:	body,
 			Spre:   Emailconfig.Mailinfo.Pre,
 			Ssuf:   Emailconfig.Mailinfo.Suf,
 			Stime:  time.Now().Format("2006年01月02日"),
-		})
-		nmail := 0
-		nmail += 1
-		fmt.Println("总共[",Enum,"]封邮件,开始发送第[",nmail,"]封邮件,发送给",touser)
+		}
+
+		t := template.New("email_template.html")
+		t, _ = template.ParseFiles("email_template.html")
+		t.Execute(buffer,Sendinfo)
+		Nmail += 1
+		fmt.Println("总共[", Enum, "]封邮件,开始发送第[", Nmail, "]封邮件,发送给", touser)
 		//send(touser, buffer.String())
+		fmt.Println(buffer.String())
 		fmt.Println(touser)
 	}
-	fmt.Println("总共[",Enum,"]封邮件,现已全部发送完毕 !")
+	fmt.Println("总共[", Enum, "]封邮件,已全部发送完毕 !")
 	fmt.Printf("\t")
-	fmt.Println("输入任意字符退出程序 ！")
+	fmt.Println("输入回车字符退出程序 ！")
 	fmt.Scanln(&Judge)
+	os.Exit(0)
+}
 
+func Emailbody(entry [][]string,row []string)(str string) {
+	lnum:=len(entry)-1
+	rnum:=len(entry[0])
+
+	//	var str string
+	for i:=0;i<rnum ;i++  {
+		for j:=0;j<lnum ;j++  {
+			var rspan,cspan int = 1,1
+			// i=23 j=1
+			if entry[j][i] != "" {
+				for z:=i;z<rnum-1 ;z++  {
+					if entry[j][z+1] =="" {
+						rspan += 1
+					}else {
+						break
+					}
+				}
+			}
+
+			if j+1<lnum {
+				if entry[j+1][i] == "" {
+					cspan += 1
+				}
+			}
+			srspan := strconv.Itoa(rspan)
+			scspan := strconv.Itoa(cspan)
+			if entry[j][i] != "" {
+				str += "<th rowspan=\"" + srspan + "\"colspan=\"" + scspan + "\">" + entry[j][i] + "</th>"
+			}
+		}
+		str=str+"<td>" + row[i] + "</td>"
+		str="<tr>"+str+"</tr>"
+	}
+	return str
 }
 
 func send(touser string, body string) {
@@ -129,7 +174,7 @@ func send(touser string, body string) {
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println(err)
-	}else {
+	} else {
 		fmt.Println("邮件发送成功 ！")
 	}
 
@@ -141,8 +186,9 @@ func readXls() (xlsfile [][]string) {
 	isheetid, _ := strconv.Atoi(Emailconfig.Xlsinfo.Sheetid)
 	Sheetname = xlsFile.GetSheetName(isheetid)
 	rows, err := xlsFile.GetRows(Sheetname)
-	Entry = rows[0]
-	Enum = len(rows)-1
+	Head, _ = strconv.Atoi(Emailconfig.Xlsinfo.Head)
+	Entry = rows[0:Head]
+	Enum = len(rows) - Head
 	return rows
 }
 
